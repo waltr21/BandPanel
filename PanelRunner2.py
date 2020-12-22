00144da89ff541609314dc4c7e295f64f716c3d6
waltr21 <waltr@mail.gvsu.edu>
2020-12-22 01:44:13 -0600

    new sin

--- PanelRunner2.py
+++ PanelRunner2.py
@@ -17,9 +17,9 @@ sceneManager = SceneManager()
 #sceneManager.addScene(Scene(ImgSizer("Mario.png", p), 8))
 #sceneManager.addScene(Scene(Disperse(10,p), 14, True))
 sceneManager.addScene(Scene(SinWave(p), 20))
-sceneManager.addScene(Scene(BouncingSquares(4,4,10,p), 10))
-sceneManager.addScene(Scene(BouncingSquares(3,3,20,p), 10))
-sceneManager.addScene(Scene(BouncingSquares(1,1,300,p), 10))
+#sceneManager.addScene(Scene(BouncingSquares(4,4,10,p), 10))
+#sceneManager.addScene(Scene(BouncingSquares(3,3,20,p), 10))
+#sceneManager.addScene(Scene(BouncingSquares(1,1,300,p), 10))
 
 
 stamp = p.getMillis()
